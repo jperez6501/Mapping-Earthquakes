7233f3fb68c649212c9a06b85d55cf80141bf731
jperez6501 <100246124+jperez6501@users.noreply.github.com>
2022-05-18 19:47:03 -0400

    Add files via upload

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,36 +1,64 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    onEachFeature:function(feature,layer) {
+      console.log(layer);
+      layer.bindPopup();
+    }
+  }).addTo(map);
 // Coordinates for each point to be used in the line.
 // Coordinates for each point to be used in the polyline.
-let line = [
-    [37.6213, -122.3790],
-    [30.2672, -97.7431],
-    [43.6532, -79.3832],
-    [40.7128, -74.0060]
-  ];
+//let line = [
+   // [37.6213, -122.3790],
+    //[30.2672, -97.7431],
+    //[43.6532, -79.3832],
+    //[40.7128, -74.0060]
+ // ];
   // Create a polyline using the line coordinates and make the line red.
 // Create a polyline using the line coordinates and make the line yellow.
-L.polyline(line, {
-    color: "blue",
-    dashArray:"5,5"
- }).addTo(map);
+//L.polyline(line, {
+   // color: "blue",
+   // dashArray:"5,5"
+ //}).addTo(map);
 // An array containing each city's location, state, and population.
 // Get data from cities.js
-let cityData = cities;
+//let cityData = cities;
  // Loop through the cities array and create one marker for each city.
 // Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        color:"purple",
-       fillColor:"#F2A529" ,
-        radius:city.population/100000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+//cityData.forEach(function(city) {
+   // console.log(city)
+   // L.circleMarker(city.location, {
+       // color:"purple",
+      // fillColor:"#F2A529" ,
+       // radius:city.population/100000
+   // })
+  //  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+ // .addTo(map);
+//});
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
