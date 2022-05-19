5ab91e620208aa88882db57198bc3b5851442a6e
jperez6501 <100246124+jperez6501@users.noreply.github.com>
2022-05-18 21:42:33 -0400

    Add files via upload

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,18 +1,18 @@
 // Add console.log to check to see if our code is working.
-//console.log("working");
+console.log("working");
 // Create the map object with center and zoom level.
 // Create the map object with a center and zoom level.
 
 // We create the tile layer that will be the background of our map.
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 // We create the dark view tile layer that will be an option for our map.
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+let satelliteStreets =  L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
@@ -20,43 +20,49 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    "Day Navigation": light,
-    "Night Navigation": dark
+    "Streets": streets,
+    "Satellite Streets": satelliteStreets
 };
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [30, 30],
-    zoom: 2,
-    layers: [light]
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [satelliteStreets]
 })
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 //streets.addTo(map);
 //dark.addTo(map);
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/jperez6501/Mapping-Earthquakes/main/torontoNeighborhoods.json";
+d3.json(torontoHoods).then(function(data){
+    console.log(data);
+L.geoJSON(data).addTo(map);
+});
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/jperez6501/Mapping-Earthquakes/main/torontoRoutes.json";
+//let torontoData = "https://raw.githubusercontent.com/jperez6501/Mapping-Earthquakes/main/torontoRoutes.json";
 // Accessing the airport GeoJSON URL
 //let airportData = "https://raw.githubusercontent.com/jperez6501/Mapping-Earthquakes/main/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 // Create a style for the lines.
-let myStyle = {
-    color: "#ffffa1",
-    weight: 2
-}
+//let myStyle = {
+    //color: "#ffffa1",
+    //weight: 2
+//}
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function (data) {
-    console.log(data);
+//d3.json(torontoData).then(function (data) {
+    //console.log(data);
 
     // Creating a GeoJSON layer with the retrieved data.
-    L.geoJSON(data, {
-        style: myStyle,
+    //L.geoJSON(data, {
+       // style: myStyle,
        // onEachFeature: function (feature, layer) {
         //    layer.binPopup("<h3> Ariline: " + Feature.properties.airline + "</h3> <hr><h3> Destination: " + Feature.properties.dst + "</h3>");
        // }
-    })
-        .addTo(map);
-});
+   // })
+       // .addTo(map);
+//});
 // Add GeoJSON data.
 //let sanFranAirport =
 //{"type":"FeatureCollection","features":[{
